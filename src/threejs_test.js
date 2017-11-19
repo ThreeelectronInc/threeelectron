fc1c3e2bf0222ee25a0b78f1520fc33af6223099
Alex Forbes <alexforbes48@gmail.com>
2017-11-19 10:07:34 +0200

    Added window resize

--- src/threejs_test.js
+++ src/threejs_test.js
@@ -52,3 +52,14 @@ var render = function () {
 };
 
 render();
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
