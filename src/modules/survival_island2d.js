49696a6a8dcaf6432f1d85c3ebd18a21970a7d6b
Denzil <denzilb55@gmail.com>
2017-12-06 20:54:28 +0200

    Try to generate sprite island but give up because you are a failure

---
+++ src/modules/survival_island2d.js
@@ -0,0 +1,87 @@
+let THREE = require('./../libs/three/three')
+
+let { BaseGame } = require('./../core/base_game')
+
+let DeityCamera = require('./../core/camera/deity')
+
+let { ipcRenderer, remote } = require('electron');
+
+let TerrainGenerator = require('./../survival2d/terrain_generator')
+
+
+class SurvivalIsland2D extends BaseGame {
+
+    constructor(tagName, fps = 0) {
+        super(tagName, fps)
+
+        this.time_elapsed = 0
+
+        this.WORLD_WIDTH = 100
+        this.WORLD_HEIGHT = 100
+    }
+
+    init() {
+
+
+
+        let ambientLight = new THREE.AmbientLight(0xcccccc);
+        this.scene.add(ambientLight);
+
+        let directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+        directionalLight.position.set(1, 1, 0.5).normalize();
+        this.scene.add(directionalLight);
+
+
+        // Camera
+        // this.lookPos.set(TerrainGenerator.world.totalBlockWidth * 0.5, 0, TerrainGenerator.world.totalBlockDepth * 0.5)
+        this.camera.position.set(0, 500, 0)
+        this.camera.lookAt(0,0,0)
+
+        this.cameraControl = new DeityCamera(this.camera, (key) => this.keyDown(key), this.mouse)
+        
+        var textureGrass = new THREE.TextureLoader().load('assets/atlas.png');
+        textureGrass.magFilter = THREE.NearestFilter;
+        textureGrass.minFilter = THREE.LinearMipMapLinearFilter;
+
+
+        var textureSand = new THREE.TextureLoader().load('assets/images/Sand.png');
+        textureSand.magFilter = THREE.NearestFilter;
+        textureSand.minFilter = THREE.LinearMipMapLinearFilter;
+
+        var textureDirt = new THREE.TextureLoader().load('assets/images/Dirt.png');
+        textureDirt.magFilter = THREE.NearestFilter;
+        textureDirt.minFilter = THREE.LinearMipMapLinearFilter;
+
+        var textureWater = new THREE.TextureLoader().load('assets/images/Water.png');
+        textureWater.magFilter = THREE.NearestFilter;
+        textureWater.minFilter = THREE.LinearMipMapLinearFilter;
+
+        let matWater = new THREE.SpriteMaterial( { map: textureWater, color: 0xffffff } );
+        let waterSprite =  new THREE.Sprite( matWater );
+
+        var textureRock = new THREE.TextureLoader().load('assets/images/Rock.png');
+
+        for (var x = 0; x < this.WORLD_WIDTH; x++) {
+            for (var y = 0; y < this.WORLD_HEIGHT; y++) {
+                let h = TerrainGenerator.getHeight(x, y)
+
+                let waterClone = waterSprite.clone()
+                waterClone.position.set(x, 0, y)
+
+                console.log(h)
+            }    
+        }
+        console.log("done")
+
+    }
+
+    deInit() {
+    }
+
+    update(delta) {
+
+    }
+
+}
+
+module.exports = SurvivalIsland2D
---
+++ src/survival2d/terrain_generator.js
@@ -0,0 +1,34 @@
+let noise = require('./../libs/improved_noise')
+let perlin = new noise.ImprovedNoise()
+
+let randSeed = Math.random(), randX = Math.random(), randZ = Math.random()
+
+
+function lerp (a,  b,  c) {
+    c = c < 0 ? 0 : c
+    c = c > 1 ? 1 : c
+    return a + c * (b - a);
+}
+
+function getHeight(x, z) {
+
+    let h = 0
+    let q = 2
+
+    for (var j = 0; j < 1; j++) {
+
+        h += (perlin.noise(randX + x / q, randZ + z / q, randSeed) + 0.4) * 0.5 * q;
+        q *= 4;
+    }
+
+  //  h = lerp(-1, h, x / 100)
+   // h = lerp(-1, h, z / 100)
+  //  h = lerp(-1, h, (WORLD.totalWidth - x) / 100)
+  //  h = lerp(-1, h, (WORLD.totalDepth - z) / 100)
+
+    return h;
+}
+
+module.exports = {
+    getHeight,
+}
