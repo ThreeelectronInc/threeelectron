12dc6d2a7bb03596999bc14f603de5d23db0cd0c
Alexander Forbes <fungu5@Alexanders-MBP.Home>
2018-05-09 06:56:12 +0200

    Material fixes and saloon cleanup

--- src/modules/saloon/saloon.js
+++ src/modules/saloon/saloon.js
@@ -22,63 +22,6 @@ class SurvivalGame extends BaseGame {
 
     constructor(tagName, fps = 0) {
         super(tagName, fps)
-
-        this.assetDirectory = 'assets'
-        this.assets = {}
-
-        this.currentIntersects = ''
-
-        this.counter = 0
-        this.planeGeometry = new THREE.PlaneGeometry(1, 1, 1)
-
-    }
-
-    loadOBJwithMTL(name,x=0,y=0,z=0){
-    
-
-        let mtlLoader = new THREE.MTLLoader();
-        mtlLoader.setTexturePath('./modules/saloon/models/');
-        mtlLoader.setPath('./modules/saloon/models/');
-
-
-        let objLoader = new THREE.OBJLoader();
-        
-        objLoader.setPath('./modules/saloon/models/');
-
-        mtlLoader.load(name+'.mtl', (materials) => {
-         
-            objLoader.setMaterials(materials);
-            materials.preload();
-         
-            objLoader.load(name+'.obj', (object) => {
-         
-                this.scene.add(object);
-                // object.position.y -= 60;
-                object.position.x = x
-                object.position.y = y
-                object.position.z = z
-                // object.children[0].geometry.name = 'beerBottle'
-                // console.log(object)
-         
-            });
-         
-        });
-    }
-
-    initLights(){
-
-        var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
-        keyLight.position.set(-100, 0, 100);
-         
-        var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
-        fillLight.position.set(100, 0, 100);
-         
-        var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
-        backLight.position.set(100, 0, -100).normalize();
-         
-        this.scene.add(keyLight);
-        this.scene.add(fillLight);
-        this.scene.add(backLight);
     }
 
     // Called when start() is called and the renderer has been initialised
@@ -94,13 +37,6 @@ class SurvivalGame extends BaseGame {
         this.camera.position.y = 2.5
         this.camera.position.z = 5
         // this.cameraControl.update(1)
-
-        this.initEntity('bee')
-        this.initEntity('skull', { posZ: 1 })
-        this.initEntity('chicken', { posX: -1 })
-        this.initEntity('chicken', { posZ: -1 })
-
-        this.makeFloor('grass', {posY: -0.5})
 
         this.initLights()            
 
@@ -171,30 +107,6 @@ class SurvivalGame extends BaseGame {
     deInit() {
     }
 
-    checkForIntersects() {
-
-        let intersects = this.raySelect()
-
-        let intersectsString = 'None'
-
-        if (intersects.length > 0) {
-            // console.log(intersects)
-            intersectsString = intersects.reduce(
-                (acc, item) => {
-                    return acc + item.object.name + ', '
-                },
-                '',
-            )
-        }
-
-        if (this.currentIntersects !== intersectsString) {
-            this.currentIntersects = intersectsString
-            this.counter++
-            this.reactHandle.forceUpdate()
-        }
-
-
-    }
 
     update(delta) {
         this.cameraControl.update(delta)
@@ -204,15 +116,6 @@ class SurvivalGame extends BaseGame {
         let { camera, renderer } = this
     }
 
-
-    onMouseDown(event) {
-        // console.log(event.button === 0)
-        if (event.button === 0) {
-            this.checkForIntersects()
-        }
-
-    }
-
     gui(guiHandle) {
 
         this.reactHandle = guiHandle // Get handle to gui to force redraws elsewhere
@@ -220,90 +123,59 @@ class SurvivalGame extends BaseGame {
         return React.createElement(
             'div',
             {},
-            `Greetings`, // Body (optional),
-            React.createElement('br'),
-            React.createElement(
-                CustomButton,
-                {
-                    name: 'Spawn bee',
-                    onClick: () => this.initEntity('bee', { posX: 1 })
-                }
-            ),
-            React.createElement(
-                CustomButton,
-                {
-                    name: 'Force redraw gui',
-                    onClick: () => { this.counter++; guiHandle.forceUpdate() }
-                }
-            ),
-            React.createElement(
-                'div',
-                null,
-                `Forced GUI updates: ${this.counter}`,
-            ),
-            React.createElement(
-                'div',
-                null,
-                `Mouse ray intersected on click: ${this.currentIntersects}`,
-            ),
+            `GUI`
 
         )
     }
 
 
 
-    getMaterial(name) {
+    loadOBJwithMTL(name,x=0,y=0,z=0){
+    
 
-        if (this.assets[name] === undefined) {
-            console.log(`Generating new asset for ${name}...`)
-            let asset = {}
-
-            asset.texture = new THREE.TextureLoader().load(`${this.assetDirectory}/${name}.png`)
-
-            asset.material = new THREE.MeshBasicMaterial({
-                map: asset.texture,
-                color: 0xffffff,
-                transparent: true,
-                side: THREE.DoubleSide,
-            })
-
-            this.assets[name] = asset
-        }
-    }
+        let mtlLoader = new THREE.MTLLoader();
+        mtlLoader.setTexturePath('./modules/saloon/models/');
+        mtlLoader.setPath('./modules/saloon/models/');
 
 
-    // Custom methods
-    initEntity(name, props = {}) {
-
-        this.getMaterial(name)
-
-        let mesh = new THREE.Mesh(
-            this.planeGeometry,
-            this.assets[name].material
-        )
-
-        this.scene.add(mesh)
-
-        let prop = (name, defaultValue) => props[name] === undefined ? defaultValue : props[name]
+        let objLoader = new THREE.OBJLoader();
         
-        mesh.scale.x = prop('scaleX', 1)
-        mesh.scale.y = prop('scaleY', 1)
-        mesh.scale.z = prop('scaleZ', 1)
+        objLoader.setPath('./modules/saloon/models/');
 
-        mesh.position.x = prop('posX', 0)
-        mesh.position.y = prop('posY', 0)
-        mesh.position.z = prop('posZ', 0)
-
-        mesh.name = name // required for picking
-        return mesh
+        mtlLoader.load(name+'.mtl', (materials) => {
+         
+            objLoader.setMaterials(materials);
+            materials.preload();
+         
+            objLoader.load(name+'.obj', (object) => {
+         
+                this.scene.add(object);
+                // object.position.y -= 60;
+                object.position.x = x
+                object.position.y = y
+                object.position.z = z
+                // object.children[0].geometry.name = 'beerBottle'
+                // console.log(object)
+         
+            });
+         
+        });
     }
 
+    initLights(){
 
-    makeFloor(name, props = {}) {
-        let floor = this.initEntity(name, props)
-
-        floor.rotateX(-Math.PI / 2)
-        return floor
+        var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+        keyLight.position.set(-100, 0, 100);
+         
+        var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+        fillLight.position.set(100, 0, 100);
+         
+        var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+        backLight.position.set(100, 0, -100).normalize();
+         
+        this.scene.add(keyLight);
+        this.scene.add(fillLight);
+        this.scene.add(backLight);
     }
 
 
